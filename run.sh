24372bce8e0618399533a3442591120c07e68efd
YongSihun <tlgns1537@naver.com>
2024-03-19 21:13:16 +0900

    #6 주석 제거 및 로깅 개선

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-# 서버 내 .env 파일로 부터 환경변수 정의
-echo "---- set environment values ----"
+echo "---- Set environment values ----"
 unamestr=$(uname)
 
 if [ $unamestr = "Linux" ]; then
@@ -15,11 +13,9 @@ echo "---- OK ----"
 echo "---- $1 ----"
 
 if [ $1 = "DOCKER-COMPOSE" ]; then
-  # 기존 서비스 종료
-  echo "---- docker compose down ----"
+  echo "---- Docker compose down ----"
   docker compose down
 
-  # 서비스 시작 (쉘 파일 내에서 export를 수행하기 때문에 환경변수가 적용됨)
-  echo "---- docker compose up ----"
+  echo "---- Docker compose up ----"
   docker compose up -d
 fi
